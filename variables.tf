338ec6f7a37b96db1fd54f75202031b966d01dc1
Quinney10 <adam.quinney1007@gmail.com>
2026-01-26 20:14:47 +0000

    Initial commit: AWS Terraform infrastructure

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+    default = "eu-west-2"
+}
+
+variable "key_name" {
+    description = "Projects key pair"
+}
+
+variable "my_ip" {
+    description = "Your public IP (00.000.00.00/32)"
+}
